47ed01475da8a276cc83b3699eddd5ed9df2f878
plugatarev <plugatarevk@gmail.com>
2023-04-05 22:45:25 +0700

    added db migration(schema, triggers, insert)

---
+++ src/main/resources/db/migration/V2__Triggers.sql
@@ -0,0 +1,160 @@
+CREATE OR REPLACE FUNCTION check_brigadier_is_worker()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM employee e, employee_category_type ect, employee_category ec
+             WHERE e.id = NEW.id AND
+                   e.employee_type = ect.id AND
+                   ect.employee_category = ec.id AND
+                   ec.name = 'worker'
+  ) THEN
+    RAISE EXCEPTION 'Brigadier must have employee_type = worker';
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER brigadier_is_worker
+BEFORE INSERT OR UPDATE ON brigadier
+FOR EACH ROW
+EXECUTE FUNCTION check_brigadier_is_worker();
+
+
+CREATE OR REPLACE FUNCTION check_brigade_employee_is_worker()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM employee e, employee_category_type ect, employee_category ec
+             WHERE e.id = NEW.worker AND
+                   e.employee_type = ect.id AND
+                   ect.employee_category = ec.id AND
+                   ec.name = 'worker'
+  ) THEN
+    RAISE EXCEPTION 'Brigade employee must have employee_type = worker';
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER brigade_employee_is_worker
+BEFORE INSERT OR UPDATE ON worker_brigade
+FOR EACH ROW
+EXECUTE FUNCTION check_brigade_employee_is_worker();
+
+
+CREATE OR REPLACE FUNCTION check_employee_is_engineer_staff()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM employee e, employee_category_type ect, employee_category ec
+             WHERE e.id = NEW.id AND
+                   e.employee_type = ect.id AND
+                   ect.employee_category = ec.id AND
+                   ec.name = 'engineering_staff'
+  ) THEN
+    RAISE EXCEPTION 'Employee must have employee_type = engineering_staff';
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER department_chief_is_engineer
+BEFORE INSERT OR UPDATE ON department_chief
+FOR EACH ROW
+EXECUTE FUNCTION check_employee_is_engineer_staff();
+
+CREATE OR REPLACE TRIGGER department_region_chief_is_engineer
+BEFORE INSERT OR UPDATE ON department_region_chief
+                            FOR EACH ROW
+                            EXECUTE FUNCTION check_employee_is_engineer_staff();
+
+CREATE OR REPLACE TRIGGER master_is_engineer
+BEFORE INSERT OR UPDATE ON master
+FOR EACH ROW
+EXECUTE FUNCTION check_employee_is_engineer_staff();
+
+
+CREATE OR REPLACE FUNCTION check_laboratory_employee_is_tester()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM employee e, employee_category_type ect, employee_category ec
+             WHERE e.id = NEW.id AND
+                   e.employee_type = ect.id AND
+                   ect.employee_category = ec.id AND
+                   ec.name = 'laboratory_tester'
+  ) THEN
+    RAISE EXCEPTION 'Laboratory employee must have employee_type = tester';
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER laboratory_employee_is_tester
+BEFORE INSERT OR UPDATE ON laboratory_employee
+FOR EACH ROW
+EXECUTE FUNCTION check_laboratory_employee_is_tester();
+
+
+CREATE OR REPLACE FUNCTION check_employee_has_attribute()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM employee e, employee_type_attribute eta
+             WHERE e.id = NEW.employee AND
+                   NEW.attribute = eta.id AND
+                   eta.employee_type = e.employee_type
+  ) THEN
+    RAISE EXCEPTION 'Employee has no such attribute';
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER employee_has_attribute
+BEFORE INSERT OR UPDATE ON employee_property
+FOR EACH ROW
+EXECUTE FUNCTION check_employee_has_attribute();
+
+
+CREATE OR REPLACE FUNCTION check_product_has_attribute()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM product p, product_category_type pct, product_type_attribute pta
+             WHERE NEW.product = p.id AND
+                   NEW.attribute = pta.id AND
+                   p.category_type = pct.id AND
+                   pta.category = pct.category
+    ) THEN
+    RAISE EXCEPTION 'Product has no such attribute';
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER product_has_attribute
+BEFORE INSERT OR UPDATE ON product_property
+FOR EACH ROW
+EXECUTE FUNCTION check_product_has_attribute();
+
+
+
+CREATE OR REPLACE FUNCTION check_status_product_process()
+RETURNS TRIGGER AS $$
+    DECLARE status_name VARCHAR(255);
+BEGIN
+SELECT product_status.name INTO status_name FROM product_status WHERE NEW.status = product_status.id;
+
+IF ((status_name = 'assembled' AND NEW.release_date IS NULL) OR
+      (status_name <> 'assembled' AND NEW.release_date IS NOT NULL))
+  THEN RAISE EXCEPTION 'Product process must has release_date only if status is assembled';
+END IF;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER status_product_process
+BEFORE INSERT OR UPDATE ON product_process
+FOR EACH ROW
+EXECUTE FUNCTION check_status_product_process();
---
+++ src/main/resources/db/migration/V3__Insert.sql
@@ -0,0 +1,180 @@
+insert into company values (1, 'Lada'),
+                           (2, 'Nada');
+
+insert into department values (1, 'motor', 1),
+                              (2, 'device', 2);
+
+insert into employee_category values (1, 'worker'),
+                                     (2, 'engineering_staff'),
+                                     (3, 'laboratory_tester');
+
+insert into employee_category_type values (1, 'engineer', 2),
+                                          (2, 'technologist', 2),
+                                          (3, 'technician', 2),
+                                          (4, 'assembler', 1),
+                                          (5, 'turner', 1),
+                                          (6, 'welder', 1),
+                                          (7, 'locksmith', 1),
+                                          (8, 'tester', 3);
+
+insert into employee_type_attribute values (1, 'education', 1),
+                                           (2, 'professional_grade', 2),
+                                           (3, 'specialization', 3),
+                                           (4, 'detail_type', 4),
+                                           (5, 'specialization', 5),
+                                           (6, 'metal_welding_type', 6),
+                                           (7, 'profile', 7);
+
+insert into employee values (1, 'Anastasia', 'Kirekova', '1234202021', 1, '10.01.2022', null),
+                            (2, 'Petr', 'Gorlanov', '3428982122', 7, '10.01.2019', null),
+                            (3, 'Maksim', 'Bobrov', '0116289821', 6, '11.12.2021', null),
+                            (4, 'Kirill', 'Greer', '2328982122', 4, '23.07.2020', '30.09.2022'),
+                            (5, 'Ivan', 'Motruhin', '5432092502', 3, '21.02.2021', null),
+                            (6, 'Andrei', 'Gurkin', '3454234523', 2, '08.04.2022', null),
+                            (7, 'Kirill', 'Flout', '3450004523', 1, '08.04.2022', '08.11.2022'),
+                            (8, 'Aleksei', 'Gurkin', '9994234523', 5, '12.12.2022', null),
+                            (9, 'Damir', 'Lend', '3454234222', 4, '28.10.2022', null),
+                            (10, 'Vladimir', 'Derop', '3454298763', 2, '23.11.2022', null),
+                            (11, 'Konstantin', 'Lok', '0951298763', 8, '21.10.2022', null),
+                            (12, 'Aleksei', 'Korjov', '0854298763', 8, '23.01.2021', null),
+                            (13, 'Ivan', 'Siroc', '0754298763', 8, '27.02.2023', null);
+
+
+insert into employee_property values (1, 2, 7, 'toolmaker'),
+                                     (2, 3, 6, 'thermite'),
+                                     (3, 4, 4, 'motor'),
+                                     (4, 9, 4, 'chassis'),
+                                     (5, 1, 1, 'nsu'),
+                                     (6, 5, 3, '5'),
+                                     (7, 6, 2, 'smth'),
+                                     (8, 7, 1, 'msu'),
+                                     (9, 8, 5, '3'),
+                                     (10, 10, 2, 'grade');
+
+
+insert into brigadier values (4),
+                             (3);
+
+
+insert into brigade values (1, 'Best', 3),
+                           (2, 'Nozzle', 3),
+                           (3, 'Import', 4);
+
+insert into worker_brigade values (1, 2, 1),
+                                  (2, 2, 2),
+                                  (3, 3, 3),
+                                  (4, 4, 1),
+                                  (5, 9, 3);
+
+insert into department_chief values (1),
+                                    (10);
+
+update department set chief = 1 where department.id = 1;
+update department set chief = 10 where department.id = 2;
+
+insert into department_region_chief values (1),
+                                           (5);
+
+insert into department_region values (1, 'First', 1, 1, 1),
+                                     (2, 'Build', 2, 3, 5);
+
+update brigade set department_region = 1 where id = 1;
+update brigade set department_region = 2 where id = 2;
+update brigade set department_region = 1 where id = 3;
+
+
+
+insert into product_category values (1, 'motorbike'),
+                                    (2, 'truck'),
+                                    (3, 'passenger_car'),
+                                    (4, 'bus'),
+                                    (5, 'agricultural_car'),
+                                    (6, 'road_construction_car');
+
+insert into product_type_attribute values (1, 'engine_capacity', 1),
+                                          (2, 'load_capacity', 2),
+                                          (3, 'doors_number', 3),
+                                          (4, 'capacity', 4),
+                                          (5, 'performance', 5),
+                                          (6, 'pressure', 6),
+                                          (7, 'engine_capacity', 3);
+
+insert into product_category_type values (1, 'Dodge Tomahawk', 1),
+                                         (2, 'Suzuki Hayabusa', 1),
+                                         (3, 'Yamaha YZF-R1', 1),
+                                         (4, 'Ducati – 1098', 1),
+                                         (5, 'kamaz-5490', 2),
+                                         (6, 'Foton Auman', 2),
+                                         (7, 'MAN TGX', 2),
+                                         (8, 'Hyundai H350', 4),
+                                         (9, 'Volvo 7900', 4),
+                                         (10, 'Maz-251', 4),
+                                         (11, 'BMW X5', 3),
+                                         (12, 'Mercedes GT', 3),
+                                         (13, 'Toyota Land Cruiser Prado', 3),
+                                         (14, 'claas', 4),
+                                         (15, 'New Holland', 4),
+                                         (16, 'niva', 4),
+                                         (17, 'Volvo Construction Equipment', 5),
+                                         (18, 'Liebherr R920', 5),
+                                         (19, 'Cat 320D2L', 5);
+
+insert into product values (1, 12, 1, 'David Belov'),
+                           (2, 5, 1, 'StroiMash'),
+                           (3, 13, 2, 'Eduard Lomov'),
+                           (4, 2, 2, 'Bloty');
+
+insert into product_property values (1, 1, 3, '5'),
+                                    (2, 2, 2, '20000'),
+                                    (3, 3, 3, '5'),
+                                    (4, 3, 7, '300');
+
+insert into product_status values (1, 'waiting'),
+                                  (2, 'assembling'),
+                                  (3, 'assembled');
+
+insert into product_process values (1, 1, 'motor manufacturing', 2, 3, '02.12.2021'),
+                                   (2, 1, 'packing', 1, 2, null),
+                                   (3, 2, 'smth', 2, 3, '02.03.2022'),
+                                   (4, 2, 'smth2', 1, 2, null),
+                                   (5, 2, 'smth3', 1, 1, null);
+
+insert into region_brigade values (1, 1, 1),
+                                  (2, 1, 2),
+                                  (3, 1, 3),
+                                  (4, 2, 3),
+                                  (5, 2, 2);
+
+insert into master values (6, 5);
+
+insert into laboratory values (1, 'crash'),
+                              (2, 'nondestructive'),
+                              (3, 'metrology');
+
+insert into laboratory_employee values (11, 1),
+                                       (12, 2),
+                                       (13, 3);
+
+insert into equipment values (1, '0213213212'),
+                             (2, '3433432121'),
+                             (3, '2344213433'),
+                             (4, '4332123231');
+
+insert into test values (1, 1, 11, 'strength'),
+                        (2, 2, 11, 'stability'),
+                        (3, 3, 12, 'solidity'),
+                        (4, 4, 13, 'permanency'),
+                        (5, 2, 12, '3433432121');
+
+insert into laboratory_order values (1, 1, 1),
+                                    (2, 1, 2),
+                                    (3, 1, 3);
+
+insert into order_test values (1, 1, 1, '12.12.2021'),
+                              (2, 1, 2, '13.12.2021'),
+                              (3, 1, 3, '14.12.2021'),
+                              (4, 1, 4, '14.12.2021'),
+                              (5, 1, 5, '30.12.2021'),
+                              (6, 2, 2, '10.10.2022'),
+                              (7, 3, 2, '13.02.2022'),
+                              (8, 3, 5, '13.02.2022');
