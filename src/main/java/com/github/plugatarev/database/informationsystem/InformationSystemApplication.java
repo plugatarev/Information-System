fbcf24ab21302fbeada74bcab5f896dba89bbf2d
plugatarev <plugatarevk@gmail.com>
2023-04-05 22:45:45 +0700

    added application properties

---
+++ src/main/java/com/github/plugatarev/database/informationsystem/InformationSystemApplication.java
@@ -0,0 +1,14 @@
+package com.github.plugatarev.database.informationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationSystemApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(InformationSystemApplication.class, args);
+    }
+
+}
