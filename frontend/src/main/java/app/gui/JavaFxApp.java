7c8171b7f46d4574ecb618ff813a4a5fa7e0607d
plugatarev <plugatarevk@gmail.com>
2023-04-28 09:59:34 +0700

    javaFX

---
+++ frontend/src/main/java/app/gui/JavaFxApp.java
@@ -0,0 +1,39 @@
+package app.gui;
+
+import app.gui.controllers.FxmlLoaderFactory;
+import app.gui.controllers.MainController;
+import app.utils.RequestExecutor;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class JavaFxApp extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        RequestExecutor requestExecutor = new RequestExecutor();
+
+        FXMLLoader mainLoader = FxmlLoaderFactory.createMainViewLoader();
+        Parent root = mainLoader.load();
+        MainController mainController = mainLoader.getController();
+        mainController.init(requestExecutor);
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Automobile company information system");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.setOnCloseRequest(event -> {
+            requestExecutor.shutdown();
+            Platform.exit();
+        });
+        stage.show();
+
+    }
+}
