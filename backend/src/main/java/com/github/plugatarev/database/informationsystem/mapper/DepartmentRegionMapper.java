8ae0be554aa8a7c1cb5aa361e7fd2068823eecc9
plugatarev <plugatarevk@gmail.com>
2023-04-28 10:01:49 +0700

    fixed cycles mapping

--- backend/src/main/java/com/github/plugatarev/database/informationsystem/mapper/DepartmentRegionMapper.java
+++ backend/src/main/java/com/github/plugatarev/database/informationsystem/mapper/DepartmentRegionMapper.java
@@ -4,6 +4,7 @@ import com.github.plugatarev.database.informationsystem.dto.DepartmentRegionDto;
 import com.github.plugatarev.database.informationsystem.entity.DepartmentRegion;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
@@ -11,8 +12,10 @@ import org.mapstruct.ReportingPolicy;
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface DepartmentRegionMapper extends IMapper<DepartmentRegion, DepartmentRegionDto> {
     @Override
+    @Mapping(target = "collectingBrigade.departmentRegion", ignore = true)
     DepartmentRegionDto toDto(DepartmentRegion departmentRegion);
 
     @Override
+    @Mapping(target = "collectingBrigade.departmentRegion", ignore = true)
     DepartmentRegion toEntity(DepartmentRegionDto departmentRegionDto);
 }
