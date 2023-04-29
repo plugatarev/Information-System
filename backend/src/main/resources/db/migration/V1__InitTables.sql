9e2c0d94cf2b889c6f0adcc474da7af45e09ae75
plugatarev <plugatarevk@gmail.com>
2023-04-29 16:45:33 +0700

    some changed schema - deleted alters

--- backend/src/main/resources/db/migration/V1__InitTables.sql
+++ backend/src/main/resources/db/migration/V1__InitTables.sql
@@ -1,27 +1,22 @@
-create table department(
-    id                 bigint primary key,
-    department_name    varchar(255) not null unique
-);
-
 create table employee_category(
-    id      bigint primary key,
+    id      bigserial primary key,
     name    varchar(255) not null unique
 );
 
 create table employee_category_type(
-    id                bigint primary key,
+    id                bigserial primary key,
     name              varchar(255) not null unique,
     employee_category bigint references employee_category(id) on delete cascade on update cascade
 );
 
 create table employee_type_attribute(
-    id                  bigint primary key,
+    id                  bigserial primary key,
     attribute_name      varchar(255) not null,
     employee_type       bigint references employee_category_type(id) on delete cascade on update cascade
 );
 
 create table employee(
-    id                      bigint primary key,
+    id                      bigserial primary key,
     first_name              varchar(255) not null,
     second_name             varchar(255) not null,
     passport                varchar(10) not null,
@@ -31,7 +26,7 @@ create table employee(
 );
 
 create table employee_property(
-    id             bigint primary key,
+    id             bigserial primary key,
     employee       bigint references employee(id) on delete cascade on update cascade,
     attribute      bigint references employee_type_attribute(id) on delete cascade on update cascade,
     value          varchar(255)
@@ -41,87 +36,91 @@ create table brigadier(
     id    bigint primary key references employee(id) on delete cascade on update cascade
 );
 
-create table brigade(
-    id                  bigint primary key,
-    brigade_name        varchar(255) not null unique,
-    brigadier           bigint references brigadier(id) on delete set null on update cascade
-);
-
-create table worker_brigade(
-    id              bigint primary key,
-    worker          bigint references employee(id) on delete cascade on update cascade,
-    brigade         bigint references brigade(id) on delete cascade on update cascade
-);
-
 create table department_chief(
     id     bigint primary key references employee(id) on delete cascade on update cascade
 );
 
-alter table department add chief bigint references department_chief(id) on delete set null on update cascade;
+create table department(
+    id                 bigserial primary key,
+    department_name    varchar(255) not null unique,
+    department_chief   bigint references department_chief(id) on delete set null on update cascade
+);
+
 
 create table department_region_chief(
     id     bigint primary key references employee(id) on delete cascade on update cascade
 );
 
 create table department_region (
-    id                  bigint primary key,
-    region_name         varchar(255) not null unique,
-    department_id       bigint references department(id) on delete cascade on update cascade,
-    collecting_brigade  bigint references brigade(id) on delete set null on update cascade,
-    chief               bigint references department_region_chief(id) on delete set null on update cascade
+    id                          bigserial primary key,
+    region_name                 varchar(255) not null unique,
+    department_id               bigint references department(id) on delete cascade on update cascade,
+    department_region_chief     bigint references department_region_chief(id) on delete set null on update cascade
 );
 
-alter table brigade add department_region bigint references department_region(id) on delete set null on update cascade;
+create table brigade(
+    id                  bigserial primary key,
+    brigade_name        varchar(255) not null unique,
+    brigadier           bigint references brigadier(id) on delete set null on update cascade,
+    department_region   bigint references department_region(id) on delete cascade on update cascade
+);
+
+create table worker_brigade(
+   id                bigserial primary key,
+   worker            bigint references employee(id) on delete cascade on update cascade,
+   brigade           bigint references brigade(id) on delete cascade on update cascade,
+   department_region bigint references department_region(id) on delete set null on update cascade
+);
 
 create table product_category(
-    id      bigint primary key,
+    id      bigserial primary key,
     name    varchar(255) not null unique
 );
 
 create table product_type_attribute(
-    id                  bigint primary key,
+    id                  bigserial primary key,
     attribute_name      varchar(255) not null,
     category            bigint references product_category(id) on delete cascade on update cascade
 );
 
 create table product_category_type(
-    id          bigint primary key,
+    id          bigserial primary key,
     name        varchar(255) not null unique,
     category    bigint references product_category(id) on delete cascade on update cascade
 );
 
 create table product(
-    id                      bigint primary key,
+    id                      bigserial primary key,
     category_type           bigint references product_category_type(id) on delete cascade on update cascade,
     manufacturer_department bigint references department(id) on delete set null on update cascade,
     customer                varchar(255) not null
 );
 
 create table product_property(
-    id          bigint primary key,
+    id          bigserial primary key,
     product     bigint references product(id) on delete cascade on update cascade,
     attribute   bigint references product_type_attribute(id) on delete cascade on update cascade,
     value       varchar(255)
 );
 
 create table product_status(
-    id   bigint primary key,
+    id   bigserial primary key,
     name varchar(255) not null unique
 );
 
 create table product_process(
-    id                  bigint primary key,
+    id                  bigserial primary key,
     product             bigint references product(id) on delete cascade on update cascade,
     description_work    text not null,
-    department_region   bigint references department_region(id) on delete set null on update cascade,
+    collecting_brigade  bigint references brigade(id) on delete cascade on update cascade,
     status              bigint references product_status(id) on delete cascade on update cascade,
     release_date        date
 );
 
 create table region_brigade(
-    id      bigint primary key,
-    region  bigint references department_region(id),
-    brigade bigint references brigade(id)
+    id      bigserial primary key,
+    region  bigint references department_region(id) on delete cascade on update cascade,
+    brigade bigint references brigade(id) on delete cascade on update cascade
 );
 
 create table master(
@@ -130,7 +129,7 @@ create table master(
 );
 
 create table laboratory(
-    id              bigint primary key,
+    id              bigserial primary key,
     laboratory_name varchar(255) not null unique
 );
 
@@ -140,25 +139,25 @@ create table laboratory_employee(
 );
 
 create table equipment(
-    id              bigint primary key,
+    id              bigserial primary key,
     serial_number   varchar(10) not null unique
 );
 
 create table test(
-    id                  bigint primary key,
+    id                  bigserial primary key,
     equipment           bigint references equipment(id) on delete cascade on update cascade,
     laboratory_employee bigint references laboratory_employee(id) on delete cascade on update cascade,
     description         text not null
 );
 
 create table laboratory_order(
-    id              bigint primary key,
+    id              bigserial primary key,
     product         bigint references product(id) on delete cascade on update cascade,
     laboratory      bigint references laboratory(id) on delete cascade on update cascade
 );
 
 create table order_test(
-    id                  bigint primary key,
+    id                  bigserial primary key,
     laboratory_order    bigint references laboratory_order(id) on delete cascade on update cascade,
     test                bigint references test(id) on delete cascade on update cascade,
     testing_date        date not null
--- backend/src/main/resources/db/migration/V3__Insert.sql
+++ backend/src/main/resources/db/migration/V3__Insert.sql
@@ -1,100 +1,106 @@
-insert into department values (1, 'motor'),
-                              (2, 'device');
+insert into department(department_name) values ('motor'),
+                                               ('device');
 
-insert into employee_category values (1, 'worker'),
-                                     (2, 'engineering_staff'),
-                                     (3, 'laboratory_tester');
+insert into employee_category(name) values ('worker'),
+                                           ('engineering_staff'),
+                                           ('laboratory_tester');
 
-insert into employee_category_type values (1, 'engineer', 2),
-                                          (2, 'technologist', 2),
-                                          (3, 'technician', 2),
-                                          (4, 'assembler', 1),
-                                          (5, 'turner', 1),
-                                          (6, 'welder', 1),
-                                          (7, 'locksmith', 1),
-                                          (8, 'tester', 3);
+insert into employee_category_type(name, employee_category) values ('engineer', 2),
+                                                                   ('technologist', 2),
+                                                                   ('technician', 2),
+                                                                   ('assembler', 1),
+                                                                   ('turner', 1),
+                                                                   ('welder', 1),
+                                                                   ('locksmith', 1),
+                                                                   ('tester', 3);
 
-insert into employee_type_attribute values (1, 'education', 1),
-                                           (2, 'professional_grade', 2),
-                                           (3, 'specialization', 3),
-                                           (4, 'detail_type', 4),
-                                           (5, 'specialization', 5),
-                                           (6, 'metal_welding_type', 6),
-                                           (7, 'profile', 7);
+insert into employee_type_attribute(attribute_name, employee_type) values ('education', 1),
+                                                                          ('professional_grade', 2),
+                                                                          ('specialization', 3),
+                                                                          ('detail_type', 4),
+                                                                          ('specialization', 5),
+                                                                          ('metal_welding_type', 6),
+                                                                          ('profile', 7);
 
-insert into employee values (1, 'Anastasia', 'Kirekova', '1234202021', 1, '10.01.2022', null),
-                            (2, 'Petr', 'Gorlanov', '3428982122', 7, '10.01.2019', null),
-                            (3, 'Maksim', 'Bobrov', '0116289821', 6, '11.12.2021', null),
-                            (4, 'Kirill', 'Greer', '2328982122', 4, '23.07.2020', '30.09.2022'),
-                            (5, 'Ivan', 'Motruhin', '5432092502', 3, '21.02.2021', null),
-                            (6, 'Andrei', 'Gurkin', '3454234523', 2, '08.04.2022', null),
-                            (7, 'Kirill', 'Flout', '3450004523', 1, '08.04.2022', '08.11.2022'),
-                            (8, 'Aleksei', 'Gurkin', '9994234523', 5, '12.12.2022', null),
-                            (9, 'Damir', 'Lend', '3454234222', 4, '28.10.2022', null),
-                            (10, 'Vladimir', 'Derop', '3454298763', 2, '23.11.2022', null),
-                            (11, 'Konstantin', 'Lok', '0951298763', 8, '21.10.2022', null),
-                            (12, 'Aleksei', 'Korjov', '0854298763', 8, '23.01.2021', null),
-                            (13, 'Ivan', 'Siroc', '0754298763', 8, '27.02.2023', null);
+insert into employee(first_name, second_name, passport, employee_type, employment_date, dismissal_date) values
+                            ('Anastasia', 'Kirekova', '1234202021', 1, '10.01.2022', null),
+                            ('Petr', 'Gorlanov', '3428982122', 7, '10.01.2019', null),
+                            ('Maksim', 'Bobrov', '0116289821', 6, '11.12.2021', null),
+                            ('Kirill', 'Greer', '2328982122', 4, '23.07.2020', '30.09.2022'),
+                            ('Ivan', 'Motruhin', '5432092502', 3, '21.02.2021', null),
+                            ('Andrei', 'Gurkin', '3454234523', 2, '08.04.2022', null),
+                            ('Kirill', 'Flout', '3450004523', 1, '08.04.2022', '08.11.2022'),
+                            ('Aleksei', 'Gurkin', '9994234523', 5, '12.12.2022', null),
+                            ('Damir', 'Lend', '3454234222', 4, '28.10.2022', null),
+                            ('Vladimir', 'Derop', '3454298763', 2, '23.11.2022', null),
+                            ('Konstantin', 'Lok', '0951298763', 8, '21.10.2022', null),
+                            ('Aleksei', 'Korjov', '0854298763', 8, '23.01.2021', null),
+                            ('Ivan', 'Siroc', '0754298763', 8, '27.02.2023', null);
 
 
-insert into employee_property values (1, 2, 7, 'toolmaker'),
-                                     (2, 3, 6, 'thermite'),
-                                     (3, 4, 4, 'motor'),
-                                     (4, 9, 4, 'chassis'),
-                                     (5, 1, 1, 'nsu'),
-                                     (6, 5, 3, '5'),
-                                     (7, 6, 2, 'smth'),
-                                     (8, 7, 1, 'msu'),
-                                     (9, 8, 5, '3'),
-                                     (10, 10, 2, 'grade');
+insert into employee_property(employee, attribute, value) values
+                                     (2, 7, 'toolmaker'),
+                                     (3, 6, 'thermite'),
+                                     (4, 4, 'motor'),
+                                     (9, 4, 'chassis'),
+                                     (1, 1, 'nsu'),
+                                     (5, 3, '5'),
+                                     (6, 2, 'smth'),
+                                     (7, 1, 'msu'),
+                                     (8, 5, '3'),
+                                     (10, 2, 'grade');
 
 
 insert into brigadier values (4),
                              (3);
 
 
-insert into brigade values (1, 'Best', 3),
-                           (2, 'Nozzle', 3),
-                           (3, 'Import', 4);
+insert into brigade(brigade_name, brigadier) values
+                           ('Best', 3),
+                           ('Nozzle', 3),
+                           ('Import', 4);
 
-insert into worker_brigade values (1, 2, 1),
-                                  (2, 2, 2),
-                                  (3, 3, 3),
-                                  (4, 4, 1),
-                                  (5, 9, 3);
+insert into worker_brigade(worker, brigade) values
+                                  (2, 1),
+                                  (2, 2),
+                                  (3, 3),
+                                  (4, 1),
+                                  (9, 3);
 
 insert into department_chief values (1),
                                     (10);
 
-update department set chief = 1 where department.id = 1;
-update department set chief = 10 where department.id = 2;
+update department set department_chief = 1 where department.id = 1;
+update department set department_chief = 10 where department.id = 2;
 
 insert into department_region_chief values (1),
                                            (5);
 
-insert into department_region values (1, 'First', 1, 1, 1),
-                                     (2, 'Build', 2, 3, 5);
+insert into department_region(region_name, department_id, department_region_chief) values ('First', 1, 1),
+                                                                        ('Build', 2, 5);
 
 update brigade set department_region = 1 where id = 1;
 update brigade set department_region = 2 where id = 2;
-update brigade set department_region = 1 where id = 3;
+update brigade set department_region = 1 where id = 2;
 
 
 
-insert into product_category values (1, 'motorbike'),
-                                    (2, 'truck'),
-                                    (3, 'passenger_car'),
-                                    (4, 'bus'),
-                                    (5, 'agricultural_car'),
-                                    (6, 'road_construction_car');
+insert into product_category(name) values
+                                    ('motorbike'),
+                                    ('truck'),
+                                    ('passenger_car'),
+                                    ('bus'),
+                                    ('agricultural_car'),
+                                    ('road_construction_car');
 
-insert into product_type_attribute values (1, 'engine_capacity', 1),
-                                          (2, 'load_capacity', 2),
-                                          (3, 'doors_number', 3),
-                                          (4, 'capacity', 4),
-                                          (5, 'performance', 5),
-                                          (6, 'pressure', 6),
-                                          (7, 'engine_capacity', 3);
+insert into product_type_attribute(attribute_name, category) values
+                                          ('engine_capacity', 1),
+                                          ('load_capacity', 2),
+                                          ('doors_number', 3),
+                                          ('capacity', 4),
+                                          ('performance', 5),
+                                          ('pressure', 6),
+                                          ('engine_capacity', 3);
 
 insert into product_category_type values (1, 'Dodge Tomahawk', 1),
                                          (2, 'Suzuki Hayabusa', 1),
@@ -121,57 +127,61 @@ insert into product values (1, 12, 1, 'David Belov'),
                            (3, 13, 2, 'Eduard Lomov'),
                            (4, 2, 2, 'Bloty');
 
-insert into product_property values (1, 1, 3, '5'),
-                                    (2, 2, 2, '20000'),
-                                    (3, 3, 3, '5'),
-                                    (4, 3, 7, '300');
+insert into product_property(product, attribute, value) values
+                                    (1, 3, '5'),
+                                    (2, 2, '20000'),
+                                    (3, 3, '5'),
+                                    (3, 7, '300');
 
-insert into product_status values (1, 'waiting'),
-                                  (2, 'assembling'),
-                                  (3, 'assembled');
+insert into product_status(name) values ('waiting'),
+                                        ('assembling'),
+                                        ('assembled');
 
-insert into product_process values (1, 1, 'motor manufacturing', 2, 3, '02.12.2021'),
-                                   (2, 1, 'packing', 1, 2, null),
-                                   (3, 2, 'smth', 2, 3, '02.03.2022'),
-                                   (4, 2, 'smth2', 1, 2, null),
-                                   (5, 2, 'smth3', 1, 1, null);
+insert into product_process(product, description_work, collecting_brigade, status, release_date) values
+                                   (1, 'motor manufacturing', 1, 3, '02.12.2021'),
+                                   (1, 'packing', 1, 2, null),
+                                   (2, 'smth', 2, 3, '02.03.2022'),
+                                   (2, 'smth2', 2, 2, null),
+                                   (2, 'smth3', 3, 1, null);
 
-insert into region_brigade values (1, 1, 1),
-                                  (2, 1, 2),
-                                  (3, 1, 3),
-                                  (4, 2, 3),
-                                  (5, 2, 2);
+insert into region_brigade(region, brigade) values (1, 1),
+                                                   (1, 2),
+                                                   (1, 3),
+                                                   (2, 3),
+                                                   (2, 2);
 
 insert into master values (6, 5);
 
-insert into laboratory values (1, 'crash'),
-                              (2, 'nondestructive'),
-                              (3, 'metrology');
+insert into laboratory(laboratory_name) values ('crash'),
+                                               ('nondestructive'),
+                                               ('metrology');
 
 insert into laboratory_employee values (11, 1),
                                        (12, 2),
                                        (13, 3);
 
-insert into equipment values (1, '0213213212'),
-                             (2, '3433432121'),
-                             (3, '2344213433'),
-                             (4, '4332123231');
+insert into equipment(serial_number) values
+                             ('0213213212'),
+                             ('3433432121'),
+                             ('2344213433'),
+                             ('4332123231');
 
-insert into test values (1, 1, 11, 'strength'),
-                        (2, 2, 11, 'stability'),
-                        (3, 3, 12, 'solidity'),
-                        (4, 4, 13, 'permanency'),
-                        (5, 2, 12, '3433432121');
+insert into test(equipment, laboratory_employee, description) values (1, 11, 'strength'),
+                                                                     (2, 11, 'stability'),
+                                                                     (3, 12, 'solidity'),
+                                                                     (4, 13, 'permanency'),
+                                                                     (2, 12, '3433432121');
 
-insert into laboratory_order values (1, 1, 1),
-                                    (2, 1, 2),
-                                    (3, 1, 3);
+insert into laboratory_order(product, laboratory) values (1, 1),
+                                                         (1, 2),
+                                                         (1, 3);
 
-insert into order_test values (1, 1, 1, '12.12.2021'),
-                              (2, 1, 2, '13.12.2021'),
-                              (3, 1, 3, '14.12.2021'),
-                              (4, 1, 4, '14.12.2021'),
-                              (5, 1, 5, '30.12.2021'),
-                              (6, 2, 2, '10.10.2022'),
-                              (7, 3, 2, '13.02.2022'),
-                              (8, 3, 5, '13.02.2022');
+insert into order_test(laboratory_order, test, testing_date) values
+                              (1, 1, '12.12.2021'),
+                              (1, 2, '13.12.2021'),
+                              (1, 3, '14.12.2021'),
+                              (1, 4, '14.12.2021'),
+                              (1, 5, '30.12.2021'),
+                              (2, 2, '10.10.2022'),
+                              (3, 2, '13.02.2022'),
+                              (3, 5, '13.02.2022');
